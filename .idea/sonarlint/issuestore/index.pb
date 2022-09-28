09c5f1cf71910e80ed0a20e389902eeef5fc6968
AndresRangelMeli <andres.rangel@mercadolibre.com.co>
2022-09-28 17:23:35 -0500

    Retrofit setup and api models

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+u
+Eapp/src/main/java/com/plcoding/jetpackcomposepokedex/ui/theme/Type.kt,b/e/bea679f8c6a16422d6d88923a835f05de8504163
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+v
+Fapp/src/main/java/com/plcoding/jetpackcomposepokedex/ui/theme/Shape.kt,f/f/ff0c9beef366acf8f801037301156b38482086c3
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
--- app/src/main/java/com/plcoding/jetpackcomposepokedex/di/AppModule.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/di/AppModule.kt
@@ -1,10 +1,33 @@
 package com.plcoding.jetpackcomposepokedex.di
 
+import com.plcoding.jetpackcomposepokedex.data.remote.PokemonApi
+import com.plcoding.jetpackcomposepokedex.repository.PokemonRepository
+import com.plcoding.jetpackcomposepokedex.utils.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun providePokemonRepository(
+        api: PokemonApi
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun providePokemonApi(): PokemonApi =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokemonApi::class.java)
+
 }
